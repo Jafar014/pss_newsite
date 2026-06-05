8c5eed46f81311c1c3788abf063ac34911fd6dc1
Jafar014 <fajaralnajiim14@gmail.com>
2026-06-05 20:50:15 +0700

    update gallery page

---
+++ resources/js/pages/gallery.tsx
@@ -0,0 +1,19 @@
+import GalleryListContent from "@/components/gallery/gallery-list-content";
+import GalleryListHeader from "@/components/gallery/gallery-list-header";
+import HomeFooter from "@/components/home/home-footer";
+import HomeNavbar from "@/components/home/home-navbar";
+import { Head } from "@inertiajs/react";
+
+export default function Gallery() {
+    return (
+        <>
+            <Head title="Galeri"/>
+            <div className="min-h-screen w-full overflow-x-hidden bg-[#f5f5f5] pt-16 md:pt-20 lg:pt-24">
+                <HomeNavbar />
+                <GalleryListHeader />  
+                <GalleryListContent/>
+                <HomeFooter />
+            </div>
+        </>
+    );
+}
