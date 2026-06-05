b097dd9698bc9f8fdc75cd9bb37ccb64f5308d73
Jafar014 <fajaralnajiim14@gmail.com>
2026-06-05 20:51:31 +0700

    update configuration

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,7 +2,6 @@ import { createInertiaApp } from '@inertiajs/react';
 import { Toaster } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { initializeTheme } from '@/hooks/use-appearance';
-import AppLayout from '@/layouts/app-layout';
 import AuthLayout from '@/layouts/auth-layout';
 import SettingsLayout from '@/layouts/settings/layout';
 
@@ -12,20 +11,12 @@ createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
     layout: (name) => {
         switch (true) {
-            case name === 'home':
-            case name === 'welcome':
-            case name === 'news':
-            case name === 'teams':
-            case name === 'competition':
-            case name === 'history':
-            case name === 'store':
-                return null;
             case name.startsWith('auth/'):
                 return AuthLayout;
             case name.startsWith('settings/'):
-                return [AppLayout, SettingsLayout];
+                return SettingsLayout;
             default:
-                return AppLayout;
+                return null;
         }
     },
     strictMode: true,
