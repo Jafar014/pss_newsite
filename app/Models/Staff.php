6766b56d06bdb07d0b0d31c6040029a485daa85d
Jafar014 <fajaralnajiim14@gmail.com>
2026-06-05 20:54:07 +0700

    add model staff

---
+++ app/Models/Staff.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Staff extends Model
+{
+    protected $table = 'staff';
+
+    protected $fillable = [
+        'team_id',
+        'full_name',
+        'role',
+        'photo_url',
+    ];
+
+    public function team()
+    {
+        return $this->belongsTo(Teams::class, 'team_id');
+    }
+}
