7eb35de595b84bf5dd6517dca4f92b43a5b6dcc8
Jafar014 <fajaralnajiim14@gmail.com>
2026-06-05 20:51:16 +0700

    update news

--- resources/js/pages/content-news.tsx
+++ resources/js/pages/content-news.tsx
@@ -1,15 +1,19 @@
 import { Head } from '@inertiajs/react';
 import HomeFooter from '@/components/home/home-footer';
 import HomeNavbar from '@/components/home/home-navbar';
-import NewsContent from '@/components/news/news_list';
+import SubNewsContent from '@/components/news/sub-news-content';
+import SubNewsHeader from '@/components/news/sub-news-header';
+import SubNewsSuggestion from '@/components/news/sub-news-suggestion';
 
-export default function NewsPage() {
+export default function Berita({ id }: { id: string }) {
     return (
         <>
             <Head title="Berita" />
             <div className="min-h-screen w-full bg-[#f5f5f5] pt-16 md:pt-20 lg:pt-24">
                 <HomeNavbar />
-                <NewsContent />
+                <SubNewsHeader/>
+                <SubNewsContent/>
+                <SubNewsSuggestion/>
                 <HomeFooter />
             </div>
         </>
