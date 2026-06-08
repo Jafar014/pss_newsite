96f05d38278c68f596cbcea73a043dad39fe82f7
Jafar014 <fajaralnajiim14@gmail.com>
2026-06-09 00:48:17 +0700

    fix https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceScheme('https');
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
         $this->configureDefaults();
     }
 
