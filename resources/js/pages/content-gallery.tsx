438cd7161a57c3c8fc88861ca68c497df794de9c
Jafar014 <fajaralnajiim14@gmail.com>
2026-06-09 16:45:21 +0700

    fix: updating responsive layout content-gallery

--- resources/js/pages/content-gallery.tsx
+++ resources/js/pages/content-gallery.tsx
@@ -12,14 +12,8 @@ export default function ContentGallery() {
             <div className="min-h-screen w-full bg-[#f5f5f5] pt-16 md:pt-20 lg:pt-24">
                 <HomeNavbar />
                 <SubGalleryHeader/>
-                <div className="w-full grid grid-cols-4">
-                    <div className="col-span-3">
-                        <SubGalleryContent/>
-                    </div>
-                    <div className="grid">
-                        <SubGallerySuggestion/>
-                    </div>
-                </div>
+                <SubGalleryContent/>
+                <SubGallerySuggestion/>
                 <HomeFooter />
             </div>
         </>
